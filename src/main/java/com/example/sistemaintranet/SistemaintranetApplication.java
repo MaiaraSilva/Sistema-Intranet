493c63bea5275a2d11516aae4ecce44b3cb6357d
Maiara da Silva Santos <maiarahsantos10@gmail.com>
2024-10-02 20:08:48 -0300

    Commit inicial

---
+++ src/main/java/com/example/sistemaintranet/SistemaintranetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaintranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaintranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaintranetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemaintranet/SistemaintranetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaintranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaintranetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
